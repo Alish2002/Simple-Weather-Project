971030c07f2c56eee7ea82fc5df505506faf1c71
Alish Kamboj <106103710+Alish2002@users.noreply.github.com>
2022-08-11 16:00:45 +0000

    Project Done

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import requests
-BaseUrl = "https://api.weatherapi.com/v1/current.json?key=70f12e3920634091b11154453220207&q="
 
-Location = input("Enter city name:")
-FinalLink = BaseUrl + Location
+BaseLink = "https://api.weatherapi.com/v1/current.json?key=70f12e3920634091b11154453220207&q="
+
+Location = input("Enter city name: ")
+FinalLink = BaseLink + Location
 
 r = requests.get(FinalLink)
 data = r.json()
-print("Current Weather: ",data["current"]["temp_c"], "C")
 
+print("Current Weather: ", data["current"]["temp_c"], "C")
 print("Wind Speed: ", data["current"]["wind_kph"])
-print("Humidity:" ,data["current"]["humidity"])
+print("Humidity:" , data["current"]["humidity"])
 print("Cloud:" , data["current"]["cloud"])
